6bcd659d95b18889ecddcc2170551c062259899c
muri27 <56584892+muri27@users.noreply.github.com>
2019-10-29 11:08:58 +0700

    Add files via upload

---
+++ (def)menghitung kecepatan (jarak, waktu).py
@@ -0,0 +1,9 @@
+#menghitung kecepatan (jarak, waktu)
+jarak = int(input())
+waktu = int(input())
+
+def kecepatan (jarak, waktu):
+    kecepatan = jarak/waktu
+    return kecepatan
+
+print(kecepatan(jarak,waktu))
